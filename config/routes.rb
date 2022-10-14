ad468ef956e98c4a4776f6251eaaad6a998133ba
Zamzam <Zamzamhassan44@gmail.com>
2022-10-14 11:23:33 +0300

    deployment

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
   # Defines the root path route ("/")
   # root "articles#index"
