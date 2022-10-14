1ce9731f7a1cdcfa9e2400ab4822b0aae64828e9
Zamzam <Zamzamhassan44@gmail.com>
2022-10-14 09:45:17 +0300

    set usestate for signup form and added post request

---
+++ client/src/components/SignUpForm.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import Error from "./Error";
+import FormField from "./FormField";
+function SignUpForm({ onLogin }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  function handleSubmit(e) {
+    e.preventDefault();
+    setErrors([]);
+    setIsLoading(true);
+    fetch("/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+        password_confirmation: passwordConfirmation
+        
+      }),
+    }).then((r) => {
+      setIsLoading(false);
+      if (r.ok) {
+        r.json().then((user) => onLogin(user));
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+      }
+    });
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="containerr">
+      <div className="contact-boxx">
+        <div className="lefty"></div>
+        <div className="righty">
+          <h2>Sign Up</h2>
+          <input
+            type="text"
+            id="username"
+            autoComplete="off"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="field1"
+            placeholder="UserNames"
+          />
+          
+        
+          <input
+            type="password"
+            id="password"
+            autoComplete="off"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="field1"
+            placeholder="Password"
+          />
+          <input
+            type="password"
+            id="password_confirmation"
+            autoComplete="new-password"
+            value={passwordConfirmation}
+            onChange={(e) => setPasswordConfirmation(e.target.value)}
+            className="field1"
+            placeholder="Confirm Your Password"
+          />
+ 
+          <button
+            className="btn1"
+            type="submit">{isLoading ? "Loading..." : "Sign Up"}
+          </button>
+          <FormField>
+            {errors.map((err) => (
+              <Error key={err}>{err}</Error>
+            ))}
+          </FormField>
+          
+        </div>
+      </div>
+      </div>
+    </form>
+  );
+}
+export default SignUpForm;
