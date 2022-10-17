a9bab20e6e60cdb7fd1dd0600de2b79c797c94ee
Zamzam <Zamzamhassan44@gmail.com>
2022-10-17 11:16:24 +0300

    initial commit

--- client/src/components/Appointment.js
+++ client/src/components/Appointment.js
@@ -34,7 +34,7 @@ function Appointment({appointment, onDeleteAppointment}) {
 <div  class="max-w-sm rounded overflow-hidden shadow-lg">
   <div class="px-6 py-4" >
     <h1>Your Appointment</h1>
-    
+
     <br></br>
   <p class="text-gray-700 text-base">Name: {table.name}</p>
     <p class="text-gray-700 text-base">Email: {table.email}</p>
@@ -42,6 +42,7 @@ function Appointment({appointment, onDeleteAppointment}) {
     <p class="text-gray-700 text-base">Appointment Time: {table.appointment_time}</p>
     <button class="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
   Edit
+  
 </button>    
 <button class="bg-pink-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleDeleteClick(table.id)}>
   Delete
