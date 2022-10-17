01dead0bb15340f8cdabe4985824ace16b4015a1
Zamzam <Zamzamhassan44@gmail.com>
2022-10-17 09:59:39 +0300

    delete

--- client/src/components/Appointment.js
+++ client/src/components/Appointment.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 
-function Appointment() {
+function Appointment({appointment, onDeleteAppointment}) {
     const [appoint, setAppoint] = useState([])
 
     useEffect(()=>{
@@ -10,22 +10,29 @@ function Appointment() {
             setAppoint(data))
     },[])
 
-    function deleteList(id) {
-        fetch(`/appointments/${id}`, {
-          method: "DELETE",
-        })
-          .then((r) => r.json())
-          .then((data) => {
-            
-          });
-      }
+   
+
+    function handleDeleteClick(id) {
+      fetch(`/appointments/${id}`, {
+        method: "DELETE",
+      })
+        .then((response) => response.json())
+        // .then(() => onDeleteAppointment(id));
+
+        .then(() => {        
+          const deleting = appoint.filter((table) => table.id !== id);   
+               setAppoint(deleting);      })
+        .catch((err) => console.log(err));    alert("delete was successful");  
+    }
+
 
   return (
     <div>
+
       {appoint.map((table)=>{
         return(
-<div class="max-w-sm rounded overflow-hidden shadow-lg">
-  <div class="px-6 py-4">
+<div  class="max-w-sm rounded overflow-hidden shadow-lg">
+  <div class="px-6 py-4" >
     <h1>Your Appointment</h1>
     <br></br>
   <p class="text-gray-700 text-base">Name: {table.name}</p>
@@ -35,8 +42,7 @@ function Appointment() {
     <button class="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
   Edit
 </button>    
-<button class="bg-pink-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-onClick={()=> deleteList(appoint.id)}> {""}
+<button class="bg-pink-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleDeleteClick(table.id)}>
   Delete
 </button>
 
